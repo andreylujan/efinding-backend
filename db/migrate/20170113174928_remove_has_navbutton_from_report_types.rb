8ccdeddd29971a5114a2cd9958261825fb67db25
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-17 16:48:47 -0300

    Fixes

--- db/migrate/20170113174928_remove_has_navbutton_from_report_types.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-class RemoveHasNavbuttonFromReportTypes < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :report_types, :has_nav_button, :boolean
-  end
-end
--- db/migrate/20170113175150_add_inspection_reference_to_reports.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-class AddInspectionReferenceToReports < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :reports, :inspection, foreign_key: true
-  end
-end
