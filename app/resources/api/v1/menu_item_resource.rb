2c27c754232aef11d79e4c741c639c23aa1b1ed6
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-23 14:15:01 -0300

    Url include

--- app/resources/api/v1/menu_item_resource.rb
+++ app/resources/api/v1/menu_item_resource.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::MenuItemResource < ApplicationResource
-	attributes :name, :admin_path, :collection_name
+	attributes :name, :admin_path, :collection_name, :url_include
 end
