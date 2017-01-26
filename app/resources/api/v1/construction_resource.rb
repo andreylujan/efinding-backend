014f2e7b27e3f25e9fe2e0eb228267a8a5ca7495
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-26 19:42:37 -0300

    String company_id

--- app/resources/api/v1/construction_resource.rb
+++ app/resources/api/v1/construction_resource.rb
@@ -2,6 +2,10 @@ class Api::V1::ConstructionResource < ApplicationResource
 	attributes :name, :company_id
 	has_one :company
 
+    def company_id
+        @model.company_id.to_s
+    end
+
 	def self.records(options = {})
     context = options[:context]
     if context[:company_id]
