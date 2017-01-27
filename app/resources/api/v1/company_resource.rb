47ebd57ada9896c26e6c46f5cdd46dedd5718943
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-27 16:19:54 -0300

    Fix

--- app/resources/api/v1/company_resource.rb
+++ app/resources/api/v1/company_resource.rb
@@ -1,5 +1,8 @@
 class Api::V1::CompanyResource < ApplicationResource
-  attributes :name
+  attributes :name, :company_name
+  def company_name
+  	@model.name
+  end
   before_save do
     @model.organization = context[:current_user].organization if @model.new_record?
   end
