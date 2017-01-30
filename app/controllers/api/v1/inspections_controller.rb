3b72682c02dd990f5bf802b72ad9148cc8c4cb1e
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-30 11:44:32 -0300

    Correctly assign signers

--- app/controllers/api/v1/inspections_controller.rb
+++ app/controllers/api/v1/inspections_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::InspectionsController < Api::V1::JsonApiController
-	before_action :doorkeeper_authorize!
+  before_action :doorkeeper_authorize!
 
-	def transition
-		@inspection = Inspection.find(params.require(:id))
-		transition_name = params.require(:transition_name)
-		@inspection.send(transition_name + "!")
-		@inspection.regenerate_all_pdfs
-		render json: JSONAPI::ResourceSerializer.new(Api::V1::InspectionResource)
-    	.serialize_to_hash(Api::V1::InspectionResource.new(@inspection, nil))
-	end
+  def transition
+    @inspection = Inspection.find(params.require(:id))
+    transition_name = params.require(:transition_name)
+    @inspection.send(transition_name + "!")
+    if @inspection.state == "first_signature_done"
+      @inspection.update_attributes! initial_signer: current_user
+    elsif inspection.state == "finished"
+      @inspection.update_attributes! final_signer: current_user
+    end
+    @inspection.regenerate_all_pdfs
+    render json: JSONAPI::ResourceSerializer.new(Api::V1::InspectionResource)
+    .serialize_to_hash(Api::V1::InspectionResource.new(@inspection, nil))
+  end
 end
