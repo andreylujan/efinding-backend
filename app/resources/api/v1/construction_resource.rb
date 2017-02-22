6f0a94cf1926853c27313c5b61fb02de21cd6a50
Pablo Lluch <pablo.lluch@gmail.com>
2017-02-22 13:52:20 -0300

    List contractors

--- app/resources/api/v1/construction_resource.rb
+++ app/resources/api/v1/construction_resource.rb
@@ -1,9 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::ConstructionResource < ApplicationResource
-	attributes :name, :company_id
+	attributes :name, :company_id, :contractors
 	has_one :company
     add_foreign_keys :company_id
     has_one :administrator    
+    def contractors
+        @model.contractors.order("name ASC").map { |u| { name: u.name, rut: u.rut, id: u.id } }
+    end
 
     filter :company_id
     
