96eab30b6c2af01bdd67f56f3925f63cb486dcb6
Pablo Lluch <pablo.lluch@gmail.com>
2017-02-24 17:13:00 -0300

    Fix

--- app/resources/api/v1/personnel_resource.rb
+++ app/resources/api/v1/personnel_resource.rb
@@ -1,3 +1,7 @@
 class Api::V1::PersonnelResource < JSONAPI::Resource
-	attributes :name, :rut
+  attributes :name, :rut
+
+  before_save do
+    @model.organization_id = context[:current_user].organization_id if @model.new_record?
+  end
 end
