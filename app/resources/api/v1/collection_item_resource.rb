55b88e701ddf8c7393825279ada1743789126f67
Pablo Lluch <pablo.lluch@gmail.com>
2017-03-02 15:43:49 -0300

    Add position to collection items

--- app/resources/api/v1/collection_item_resource.rb
+++ app/resources/api/v1/collection_item_resource.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::CollectionItemResource < ApplicationResource
-	attributes :name, :code
+	attributes :name, :code, :position
 	add_foreign_keys :parent_item_id, :collection_id
 	has_one :collection
 	has_one :parent_item
