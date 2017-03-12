b967a5145416709182a74ddf468448c31466df87
Pablo Lluch <pablo.lluch@gmail.com>
2017-03-12 00:06:34 -0300

    Contractors again

--- app/controllers/api/v1/constructions_controller.rb
+++ app/controllers/api/v1/constructions_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::ConstructionsController < Api::V1::JsonApiController
       if params[:fields].present?
         fields = params[:fields]
       end
-      fields[:constructions] = "name,company_id,code,administrator_id,expert_id,supervisor_id,expert,administrator"
+      fields[:constructions] = "name,company_id,code,administrator_id,expert_id,supervisor_id,expert,administrator,contractors"
       params[:fields] = fields
     end
     super
