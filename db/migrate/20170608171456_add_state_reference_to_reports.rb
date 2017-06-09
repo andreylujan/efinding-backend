146298d29476bd20a2a8f00f99866ba47f50ac32
Pablo Lluch <pablo.lluch@gmail.com>
2017-06-09 16:39:51 -0400

    Migration fixes

--- db/migrate/20170608171456_add_state_reference_to_reports.rb
+++ db/migrate/20170608171456_add_state_reference_to_reports.rb
@@ -3,8 +3,7 @@ class AddStateReferenceToReports < ActiveRecord::Migration[5.0]
     add_reference :reports, :state, foreign_key: true
     Report.with_deleted.all.each do |report|
       if report.state.nil?
-        report.state = report.report_type.initial_state
-        report.save!
+        report.update_column :state_id, report.report_type.initial_state_id
       end
     end
     change_column :reports, :state_id, :integer, null: false
--- db/migrate/20170608174609_add_state_reference_to_sections.rb
+++ db/migrate/20170608174609_add_state_reference_to_sections.rb
@@ -2,7 +2,8 @@ class AddStateReferenceToSections < ActiveRecord::Migration[5.0]
   def change
     add_reference :sections, :state, foreign_key: true
     Section.all.each do |section|
-      section.state = section.report_type.initial_state
+      report_type = ReportType.find(section.report_type_id)
+      section.state = report_type.initial_state
       section.save!
     end
     remove_column :sections, :report_type_id
