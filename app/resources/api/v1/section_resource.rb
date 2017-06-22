39c64fe77542b361f69a98b08166b8117b23371a
Pablo Lluch <pablo.lluch@gmail.com>
2017-06-21 22:54:02 -0400

    Add missing foreign keys

--- app/resources/api/v1/section_resource.rb
+++ app/resources/api/v1/section_resource.rb
@@ -3,7 +3,7 @@ class Api::V1::SectionResource < ApplicationResource
 
   attributes :name, :position, :config, :section_type
   has_many :data_parts, always_include_linkage_data: false
-  # add_foreign_keys :state_id
+  add_foreign_keys :state_id
 
   def custom_links(options)
     { self: nil }
