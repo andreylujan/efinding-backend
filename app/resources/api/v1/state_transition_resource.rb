0e222a546530a2420868874ea7f5257784d6ba2c
Pablo Lluch <pablo.lluch@gmail.com>
2017-06-21 23:07:13 -0400

    Add more foreign keys

--- app/resources/api/v1/state_transition_resource.rb
+++ app/resources/api/v1/state_transition_resource.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
-class Api::V1::StateTransitionResource < JSONAPI::Resource
+class Api::V1::StateTransitionResource < ApplicationResource
   attributes :name
   has_one :previous_state
   has_one :next_state
+  add_foreign_keys :previous_state_id, :next_state_id
 end
