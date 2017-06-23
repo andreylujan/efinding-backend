ce1be4b517a9df2075dab14f00d76068ca8ad713
Pablo Lluch <pablo.lluch@gmail.com>
2017-06-23 13:52:45 -0400

    Underscore

--- app/resources/api/v1/data_part_resource.rb
+++ app/resources/api/v1/data_part_resource.rb
@@ -7,7 +7,7 @@ class Api::V1::DataPartResource < ApplicationResource
   has_one :collection
 
   def data_part_type
-    @model.type
+    @model.type.underscore
   end
 
   def self.records(options = {})
