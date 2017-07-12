017dc3ae45db3670d72914573e2f97cfa3e9c7b6
Pablo Lluch <pablo.lluch@gmail.com>
2017-07-12 15:36:12 -0400

    More fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
 
 	def invite_email(invitation)
 		@invitation = invitation
-		@url = Rails.env.production? ? "http://50.16.161.152/efinding/admin/#/signup" : "http://50.16.161.152/efinding-staging/admin/#/signup"
+		@url = @invitation.organization.echeckit? ? "http://50.16.161.152/productos/echeckit-staging/#/signup" : "http://50.16.161.152/productos/efinding-staging/#/signup"
 		mail(to: @invitation.email, subject: "Únete a #{invitation.role.organization.name}",
 			from: "#{invitation.role.organization.name} Admin<solutions@ewin.cl>",
 			template_name: "invite_#{invitation.role.organization.app_name}")
