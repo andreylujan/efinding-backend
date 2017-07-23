976c2a4ac89164fe01b6b3d7fc90131b314a838a
Pablo Lluch <pablo.lluch@gmail.com>
2017-07-22 21:01:37 -0400

    Change condition on send task job

--- app/models/report.rb
+++ app/models/report.rb
@@ -125,7 +125,7 @@ class Report < ApplicationRecord
   before_save :check_dynamic_changes, on: [ :update ]
 
   after_commit :generate_pdf # , on: [ :create ]
-  after_commit :send_task_job, on: [ :create ]
+  after_commit :send_task_job, on: [ :update ]
   
   validate :limit_date_cannot_be_in_the_past, on: :create
   validate :valid_state_transition, on: [ :update ]
@@ -444,7 +444,7 @@ class Report < ApplicationRecord
 
   
   def send_task_job
-    if self.assigned_user.present?
+    if changes["assigned_user_id"].present? and self.assigned_user.present?
       SendTaskJob.set(wait: 1.second, queue: ENV['PUSH_QUEUE'] || 'efinding_push').perform_later(self.id.to_s)
     end
   end
