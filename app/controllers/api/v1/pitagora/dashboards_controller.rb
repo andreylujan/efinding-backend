fcd1dba062a77bba09206af3657c3d730caa9e1d
Pablo Lluch <pablo.lluch@gmail.com>
2017-07-24 17:25:06 -0400

    Fix

--- app/controllers/api/v1/pitagora/dashboards_controller.rb
+++ app/controllers/api/v1/pitagora/dashboards_controller.rb
@@ -5,7 +5,10 @@ class Api::V1::Pitagora::DashboardsController < Api::V1::JsonApiController
 
   def inspections
 
-    activity_groups = Collection.find(28).collection_items.map do |item|
+    risks = Collection.find(28).collection_items.map do |item|
+      item.name
+    end
+    activity_groups = Collection.find(26).collection_items.map do |item|
       item.name
     end
     reportes_por_grupo = Report.joins(creator: :role).where(roles: { organization_id: 5 })
@@ -15,7 +18,7 @@ class Api::V1::Pitagora::DashboardsController < Api::V1::JsonApiController
       group.activity_group
     end.map do |activity_group, reports|
       subgroup = {}
-      activity_groups.each do |ag|
+      risks.each do |ag|
         subgroup[ag] = 0
       end
       reports.group_by { |r| r.risk }
@@ -26,15 +29,31 @@ class Api::V1::Pitagora::DashboardsController < Api::V1::JsonApiController
         grupo_actividad: activity_group,
         por_riesgo: subgroup.map do |risk, risk_val|
           {
-            grado_riesgo: risk,
+            name: risk,
             num_reports: risk_val
           }
         end
       }
     end
+    categories = activity_groups
+    riesgos = []
+    risks.each do |risk|
+      riesgos << {
+          name: risk,
+          data: []
+        }
+    end
+    reportes_por_grupo.each do |grupo_actividad|
+      grupo_actividad[:por_riesgo].each_with_index do |riesgo, idx|
+        riesgos[idx][:data] << riesgo[:num_reports]
+      end
+    end
     dashboard_info = {
       id: SecureRandom.uuid,
-      reportes_por_grupo: reportes_por_grupo
+      reportes_por_grupo:  {
+        grupos_actividad: categories,
+        grados_riesgo: riesgos
+      }
 
     }
     dashboard = ::Pitagora::InspectionDashboard.new dashboard_info
