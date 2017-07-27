6cf1c585c8ab466a8db861a956513d705f2af0c6
Pablo Lluch <pablo.lluch@gmail.com>
2017-07-27 15:27:19 -0400

    Add missing migration

---
+++ db/migrate/20170727192312_add_default_title_and_default_subtitle_to_report_types.rb
@@ -0,0 +1,6 @@
+class AddDefaultTitleAndDefaultSubtitleToReportTypes < ActiveRecord::Migration[5.0]
+  def change
+    add_column :report_types, :default_title, :text, null: false, default: "Sin título"
+    add_column :report_types, :default_subtitle, :text, null: false, default: "Sin subtítulo"
+  end
+end
