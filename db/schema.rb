9b93f56a5fbe1c19dc3dd9c198f17f35620b7ccd
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-01 13:11:56 -0400

    Contractors fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# -*- encoding : utf-8 -*-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170731221204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
   enable_extension "postgis"
   enable_extension "uuid-ossp"
 
