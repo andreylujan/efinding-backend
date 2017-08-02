7081444cb3d124a3058dba402a6559554ed48b94
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-02 13:51:25 -0400

    Contractor default order

--- app/resources/api/v1/contractor_resource.rb
+++ app/resources/api/v1/contractor_resource.rb
@@ -5,10 +5,13 @@ class Api::V1::ContractorResource < JSONAPI::Resource
   before_save do
     @model.organization = context[:current_user].organization if @model.new_record?
   end
-  
+
   def self.records(options = {})
     context = options[:context]
     current_user = context[:current_user]
-    Contractor.where(organization_id: current_user.organization_id)
+    contractors = Contractor.where(organization_id: current_user.organization_id)
+    if not options.has_key? :sort_criteria or options[:sort_criteria].blank?
+      contractors = contractors.order("contractors.name ASC")
+    end
   end
 end
