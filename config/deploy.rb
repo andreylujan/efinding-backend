7b4b7a017cf03c2dda9f0ff836a495890021f3ca
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-02 17:21:49 -0400

    Cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 # config valid only for current version of Capistrano
-lock '3.7.2'
+lock '3.9.0'
 
 set :application, 'api'
 set :repo_url, 'git@github.com:andreylujan/efinding-backend.git'
