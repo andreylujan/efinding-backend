978045c8037ff4c7ada5f0354b15f00091f7248e
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-07 12:47:55 -0400

    Table column

--- app/admin/table_column.rb
+++ app/admin/table_column.rb
@@ -9,6 +9,17 @@ ActiveAdmin.register TableColumn do
   # filter :current_sign_in_at
   # filter :sign_in_count
 
-
-
+  form do |f|
+  	f.input :organization
+  	f.input :field_name
+  	f.input :column_name
+  	f.input :position
+  	f.input :relationship_name
+  	f.input :data_type
+  	f.input :collection_name
+  	f.input :collection_source
+  	f.input :headers, as: :text, input_html: { value: table_column.headers.to_json, class: 'jsoneditor-target' }
+  	actions
+  end
+	
 end
