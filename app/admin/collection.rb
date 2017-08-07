b10523b3925c91470ffcddfb02399f55f312b56f
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-07 12:42:44 -0400

    Collection

---
+++ app/admin/collection.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Collection do
+  controller do
+    define_method :permitted_params do
+      params.permit!
+    end
+  end
+
+  # filter :email
+  # filter :current_sign_in_at
+  # filter :sign_in_count
+
+
+
+end
