e40c650892651d340d8f440057983895426ca22a
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-18 10:46:19 -0300

    Fix

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -23,20 +23,7 @@ class UploadPdfJob < ApplicationJob
     report.ignore_pdf = true
     map_type = report.creator.organization.map_type
 
-    if report.initial_location.present?
-      initial_location_image = open("http://maps.googleapis.com/maps/api/staticmap?&maptype=#{map_type}&zoom=15&size=400x250&markers=size:" +
-                                    "mid%7Ccolor:green%7C#{report.initial_location.lonlat.y},#{report.initial_location.lonlat.x}&key=AIzaSyAxdoh8VjK53CCCYNmGx0RGuompeK-ejzc")
-      report.initial_location_image = initial_location_image
-      report.save!
-    end
-
-    if report.final_location.present?
-      final_location_image = open("http://maps.googleapis.com/maps/api/staticmap?&maptype=#{map_type}&zoom=15&size=400x250&markers=size:" +
-                                  "mid%7Ccolor:orange%7C#{report.final_location.lonlat.y},#{report.final_location.lonlat.x}&key=AIzaSyAxdoh8VjK53CCCYNmGx0RGuompeK-ejzc")
-      report.final_location_image = final_location_image
-      report.save!
-    end
-
+  
 
     report.ignore_pdf = false
     report.report_type.pdf_templates.each do |template|
@@ -45,17 +32,19 @@ class UploadPdfJob < ApplicationJob
                                     locals: { report: report })).force_encoding("UTF-8")
         pdf = WickedPdf.new.pdf_from_string(html, zoom: 0.75)
         file = Tempfile.new('pdf', encoding: 'ascii-8bit')
+        html_file = Tempfile.new('html', encoding: 'UTF-8')
       rescue => e
         return
       end
 
       begin
         file.write(pdf)
-
-        report.pdf = file
+        pdf = Pdf.find_or_initialize_by(report_id: report.id, pdf_template_id: template.id)
         report.pdf_uploaded = true
-        report.save!
-        # report.html = html_file
+        html_file.write(html)
+        pdf.pdf = file
+        pdf.html = html_file
+        pdf.save!
         report.save!
       ensure
         file.close
