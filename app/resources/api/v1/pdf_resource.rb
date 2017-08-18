69f3dc812075dc8aca2e05bb67c221ec5ca1fcdf
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-18 15:59:43 -0300

    Pdf HTML fix

--- app/resources/api/v1/pdf_resource.rb
+++ app/resources/api/v1/pdf_resource.rb
@@ -4,7 +4,7 @@ class Api::V1::PdfResource < ApplicationResource
 
 	def pdf_html
 		if @model.uploaded?
-			"<input ng-if=\"report[‘pdfUploaded’]\" class=\"text-center\" type=\"image\" src=\"https://s3-sa-east-1.amazonaws.com/efinding/icons/PDF.png\" uib-tooltip=\"Descargar PDF\" tooltip-placement=\"bottom\" style=\"width: 15px\" data-pdf=\"#{@model.pdf_url}\" ng-click=\"downloadPdf($event)\"/>"
+			"<a href=\"#{@model.pdf_url}\" target=\"_blank\"><input class=\"text-center\" type=\"image\" src=\"https://s3-sa-east-1.amazonaws.com/efinding/icons/PDF.png\" uib-tooltip=\"Descargar PDF\" tooltip-placement=\"bottom\" style=\"width: 15px\" data-pdf=\"\"/></a>"
 		else
 			"<input ng-if=\"!report[‘pdfUploaded’]\" class=\"text-center\" type=\"image\" src=\"http://dhg7r6mxe01qf.cloudfront.net/icons/admin/denied2.png\" uib-tooltip=\"PDF no disponible\" tooltip-placement=\"bottom\" style=\"width: 20px;\" />"
 		end
