f7ea959b7942a275058744f882928c4df96526fe
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-23 20:14:04 -0300

    Editable

--- app/resources/api/v1/state_resource.rb
+++ app/resources/api/v1/state_resource.rb
@@ -1,10 +1,25 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::StateResource < ApplicationResource
-	attributes :name, :color, :show_pdf
-	has_many :previous_states
-	has_many :next_states
-	has_many :sections
-	has_many :outgoing_transitions
-	has_one :report_type
-	add_foreign_keys :report_type_id
+  attributes :name, :color, :show_pdf, :editable
+  has_many :previous_states
+  has_many :next_states
+  has_many :sections
+  has_many :outgoing_transitions
+  has_one :report_type
+  add_foreign_keys :report_type_id
+
+  def editable
+    # VERY temp fix for IDD before role rework
+    # Resolviendo
+    current_user = context[:current_user]
+    if @model.id == 13
+    	# Resolutos
+      current_user.role_id == 14
+    # Terminando
+  	elsif @model.id == 15
+  		current_user.role_id == 13
+  	else
+  		true
+    end
+  end
 end
