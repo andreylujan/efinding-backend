8dca57d060a42bbc7bb2b27d340c7e8ce09784bd
Pablo Lluch <pablo.lluch@gmail.com>
2017-08-23 13:47:28 -0300

    Add helper method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -18,32 +18,39 @@
 
 class Organization < ApplicationRecord
 
-    has_many :roles
-    has_many :report_types
-    has_many :table_columns, -> { order(position: :asc) }
-    has_many :menu_sections, -> { order(position: :asc) }
-    has_many :companies, dependent: :destroy
-    has_many :reports, dependent: :destroy
-    has_many :users, through: :roles
-    has_many :collections
-    belongs_to :checklist
-    has_many :checklists
-    has_many :personnel_types
-    belongs_to :default_report_type, class_name: :ReportType, foreign_key: :default_report_type_id
+  has_many :roles
+  has_many :report_types
+  has_many :table_columns, -> { order(position: :asc) }
+  has_many :menu_sections, -> { order(position: :asc) }
+  has_many :companies, dependent: :destroy
+  has_many :reports, dependent: :destroy
+  has_many :users, through: :roles
+  has_many :collections
+  belongs_to :checklist
+  has_many :checklists
+  has_many :personnel_types
+  belongs_to :default_report_type, class_name: :ReportType, foreign_key: :default_report_type_id
 
-    enum map_type: [ :roadmap, :satellite ]
-    enum app_name: [ :echeckit, :efinding ]
+  enum map_type: [ :roadmap, :satellite ]
+  enum app_name: [ :echeckit, :efinding ]
 
-    validates :default_admin_path, presence: true
-    
-    def checklist_id
-        if checklist.present?
-            checklist.id
-        end
+  validates :default_admin_path, presence: true
+
+  def regenerate_all_pdfs
+    reports.each do |report|
+      report.pdfs.destroy_all
+      report.regenerate_pdf(true)
     end
+  end
 
-    def users
-    	User.joins(:role)
-    	.where(roles: { organization_id: self.id })
+  def checklist_id
+    if checklist.present?
+      checklist.id
     end
+  end
+
+  def users
+    User.joins(:role)
+    .where(roles: { organization_id: self.id })
+  end
 end
