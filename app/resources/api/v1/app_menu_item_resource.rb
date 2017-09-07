1225bb1e408f754f652d744425281196bd270774
Pablo Lluch <pablo.lluch@gmail.com>
2017-09-07 11:20:36 -0300

    Filter name fixed

--- app/resources/api/v1/app_menu_item_resource.rb
+++ app/resources/api/v1/app_menu_item_resource.rb
@@ -1,5 +1,5 @@
 class Api::V1::AppMenuItemResource < ApplicationResource
 	add_foreign_keys :organization_id
-	attributes :name, :url_include, :filter_assigned,
+	attributes :name, :url_include, :filter_assigned_user,
 		:filter_creator, :icon
 end
