d52f13b7ee9ed040399345535ff455384d2c7007
Pablo Lluch <pablo.lluch@gmail.com>
2017-09-11 12:51:55 -0300

    Change back

--- app/jobs/change_state_job.rb
+++ app/jobs/change_state_job.rb
@@ -11,21 +11,21 @@ class ChangeStateJob < ApplicationJob
       suffix = report.state_id == 18 ? "archivado, ya que es competencia de otro ente" : "resuelto"
       message = "El reclamo #{title.downcase} ha sido #{suffix}"
 
-      conn = Faraday.new(:url => "http://190.0.158.2:8083")
+      conn = Faraday.new(:url => "http://ec2-54-88-114-83.compute-1.amazonaws.com")
 
       begin
         response = conn.post do |req|
-          req.url '/api/index.php/api/reclamo/send_push_notification'
+          req.url '/idd/index.php/api/reclamo/send_push_notification'
           req.body = {
             email: email,
             title: title,
             message: message
           }
         end
-        RequestLog.create! organization_id: 6, url: "http://190.0.158.2:8083/api/index.php/api/reclamo/send_push_notification", status_code: response.status,
+        RequestLog.create! organization_id: 6, url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/idd/index.php/api/reclamo/send_push_notification", status_code: response.status,
           response_body: response.body
       rescue => e
-        RequestLog.create! organization_id: 6, url: "http://190.0.158.2:8083/api/index.php/api/reclamo/send_push_notification", status_code: 0,
+        RequestLog.create! organization_id: 6, url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/idd/index.php/api/reclamo/send_push_notification", status_code: 0,
           error_messages: [ e.message ]
       end
     end
