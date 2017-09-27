5471a63ba73c7f67b66b1e669c1316fa6d0ef31e
Pablo Lluch <pablo.lluch@gmail.com>
2017-09-27 12:24:15 -0300

    Fix correo jefes de terreno

--- app/controllers/api/v1/inspections_controller.rb
+++ app/controllers/api/v1/inspections_controller.rb
@@ -40,12 +40,14 @@ class Api::V1::InspectionsController < Api::V1::JsonApiController
   def transition
     @inspection = Inspection.find(params.require(:id))
     transition_name = params.require(:transition_name)
-    @inspection.send(transition_name + "!")
-    if @inspection.state == "first_signature_done"
-      @inspection.update_attributes! initial_signer: current_user
-    elsif @inspection.state == "finished"
-      @inspection.update_attributes! final_signer: current_user
+    if transition_name == "sign"
+      if @inspection.state == "first_signature_pending"
+        @inspection.initial_signer = current_user
+      elsif @inspection.state == "final_signature_pending"
+        @inspection.final_signer = current_user
+      end
     end
+    @inspection.send(transition_name + "!")
     @inspection.regenerate_all_pdfs
     render json: JSONAPI::ResourceSerializer.new(Api::V1::InspectionResource)
     .serialize_to_hash(Api::V1::InspectionResource.new(@inspection, nil))
