cd9ad1015375b778720a2d180a8cc26918427ddf
Santiago Morales <smorales@bildchile.com>
2018-01-12 18:38:30 -0300

    fix

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -124,7 +124,7 @@ class Api::V1::DashboardController < Api::V1::JsonApiController
        .order("reports.created_at DESC")
        .map do |group|{
          num_reports: group.num_reports,
-         week: week.strftime('%U') + 1
+         week_code: group.week.strftime('%U') + 1
        }
      end
 
