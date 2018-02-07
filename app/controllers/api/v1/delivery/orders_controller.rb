a9be651496fbd55644f42061763446ebdca15788
Santiago Morales <smorales@bildchile.com>
2018-02-07 17:18:55 -0300

    fix order_scheduled_date error

--- app/controllers/api/v1/delivery/orders_controller.rb
+++ app/controllers/api/v1/delivery/orders_controller.rb
@@ -88,7 +88,7 @@ class Api::V1::Delivery::OrdersController < ApplicationController
 
       is_scheduled = params[:order_is_scheduled]
       scheduled_at = nil
-      if is_scheduled
+      if is_scheduled and params[:order_scheduled_date].nil?
         report.scheduled_at = DateTime.parse(params[:order_scheduled_date])
         .change(offset: "-0300")
         report.dynamic_attributes["48"] = {
