112c8cf40e13345ca40c15c441b818cc20b4dabe
Santiago Morales <smorales@bildchile.com>
2018-02-07 11:25:28 -0300

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :repo_url, 'git@github.com:andreylujan/efinding-backend.git'
 set :branch, 'master'
 
 set :ssh_options, {
-  nandilagos [11:23]
  keys: [ "#{ENV['HOME']}/.ssh/echeckit_production.pem", "./keys/echeckit_production.pem" ],
  forward_agent: true,
  auth_methods: ["publickey"]
