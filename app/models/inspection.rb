c43d0a3af548149270b6ef9f7c7ad44ceccd6e73
Santiago Morales <smorales@bildchile.com>
2018-03-09 12:06:37 -0300

    fix pitagora mail

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -210,6 +210,18 @@ class Inspection < ApplicationRecord
     InspectionPdfJob.set(wait: 5.seconds, queue: ENV['REPORT_QUEUE'] || 'echeckit_report').perform_later(self.id.to_s)
   end
 
+  def signature_pending
+    users = [ inspection.construction.administrator ]
+    users.each do |user|
+      Rails.logger.info "Solicitud de firma : #{user}"
+      UserMailer.delay_for(10.seconds, queue: ENV['EMAIL_QUEUE'] || 'echeckit_email')
+      .inspection_email(inspection.id, user, "Solicitud de firma - #{inspection.construction.name}",
+                        "#{inspection.construction.supervisor.name} ha enviado una nueva inspección para ser firmada " +
+                        "en la obra #{inspection.construction.name}. " +
+                        "Para realizar la firma, puedes ingresar a #{ENV['ADMIN_URL']}/#/efinding/inspecciones/lista")
+    end
+  end
+
   def check_state
     if state == "first_signature_done" and reports.where(state: "unchecked").count == 0
       self.resolved_at = DateTime.now
