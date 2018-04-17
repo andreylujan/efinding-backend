48bc0f7a1ce18ffeb87c5b1a3308112035405553
Santiago Morales <smorales@bildchile.com>
2018-04-17 10:01:12 -0300

    fix mailer

--- app/mailers/user_send_grid_mailer.rb
+++ app/mailers/user_send_grid_mailer.rb
@@ -3,15 +3,9 @@ class UserSendGridMailer < ApplicationMailer
 
 	def invite_email(invitation)
 		@invitation = invitation
-		if @invitation.organization.echeckit?
-			url_suffix = Rails.env.production? ? "echeckit" : "echeckit-staging"
-		else
-			url_suffix = Rails.env.production? ? "efinding" : "efinding-staging"
-		end
-		@url = "#{ENV['BASE_ADMIN_URL']}/#{url_suffix}/admin/#/signup"
+		@url = "#{ENV['ADMIN_URL']}/#/signup"
 		mail(to: @invitation.email, subject: "Únete a #{invitation.role.organization.name}",
-			from: "#{invitation.role.organization.name} Admin<solutions@ewin.cl>",
-			template_name: "invite_#{invitation.role.organization.app_name}")
+			from: "#{invitation.role.organization.name} Admin<solutions@ewin.cl>")
 	end
 
 	def confirmation_email(user)
@@ -20,60 +14,45 @@ class UserSendGridMailer < ApplicationMailer
 			from: "#{user.organization.name} Admin<solutions@ewin.cl>")
 	end
 
-	def pausa_email(report)
-		@reporte =  report
-
-		case Integer(report.organization_id)
-		when 11
-			@d = "116"
-			if report.dynamic_attributes.dig(@d, "value").present?
-				@solicitud = report.dynamic_attributes.dig(@d, "value")
-				@mantencion = report.dynamic_attributes.dig("115", "value")
-				@category = report.dynamic_attributes.dig("113", "value")
-				@place = report.dynamic_attributes.dig("114", "value")
-				mail(to: 'pruebas.bild@gmail.com, jguerrero80@gmail.com,leslienilu@hotmail.com,lesliepassalacqua@pausaperu.pe, Luisaleon@pausaperu.pe', subject: "PAUSA PERÚ - Se generado una Solicitud de Repuesto",
-					from: "Admin<solutions@ewin.cl>")
-			end
-		when 8
-			@d = "122"
-			if report.dynamic_attributes.dig(@d, "value").present?
-				@solicitud = report.dynamic_attributes.dig(@d, "value")
-				@mantencion = report.dynamic_attributes.dig("121", "value")
-				@category = report.dynamic_attributes.dig("101", "value")
-				@place = report.dynamic_attributes.dig("102", "value")
-				mail(to: 'pruebas.bild@gmail.com, cristianquilaman@pausachile.cl, carolakrebs@pausachile.cl, jorgeguerrero@pausachile.cl', subject: "PAUSA CHILE - Se generado una Solicitud de Repuesto",
-					from: "Admin<solutions@ewin.cl>")
-			end
-		end
-		return
-	end
-
-	def manflas_email(report)
-		@file = File.read('./email_manflas.json')
-		@json = JSON.parse(file)
-		@reporte = report
-		@s = "subtitle"
-		@u = "assigned_user"
-		if Integer(report.organization_id) == 3
-			if repor.dynamic_attributes.dig(@s, "value") != nil
-				@user = report.dynamic_attributes.dig(@u, "value")
-				if @user != nil and @user != ""
-					 @a = repor.dynamic_attributes.dig(@s, "value")
-					 @a.downcase
-					 @area, @category = @a.split('/')
-					 Rails.logger.debug "Mails: smorales@bildchile.com #{@user[:email]} #{@json[:@area][:@category]}"
-					 mail(to: 'smorales@bildchile.com' + @user[:email],
-						 subject: "Manflas - Se generado un reporte", cc: @json[:@area][:@category],
-						  from: "Admin<solutions@ewin.cl>")
-				end
-			end
-		end
-	end
-
-
 	def reset_password_email(user)
-    @user = user
+		@user = user
 		mail(to: @user.email, subject: 'Recuperación de contraseña')
+	end
+
+	def checklist_email(checklist_id, user, subject, message)
+		report = ChecklistReport.find(checklist_id)
+		@user = user
+		@message = message
+		@url = report.pdf_url
+		mail(to: @user.email, subject: subject)
+	end
+
+	def manflas_email(report_id, user)
+		report = Report.find(report_id)
+		@reporte = report
+		if Integer(report.creator.organization_id) == 3
+			if report.dynamic_attributes["subtitle"]!= nil
+				a = report.dynamic_attributes["subtitle"].downcase
+				area, category = a.split('/')
+				file = File.read('./email_manflas.json')
+				mailJson = JSON.parse(file)
+				@user = user
+				@message = "Se ha generado un nuevo reporte"
+				@url = report.pdf_url
+				area.downcase
+				category.downcase
+				cc = mailJson.fetch(area).fetch(category)
+				mail(to:@user.email, subject:"Manflas - se ha generado un nuevo reporte", cc:cc, from: "Admin<solutions@ewin.cl>")
+			end
+		end
+	end
+
+	def report_email(report_id, user, subject, message)
+		report = Report.find(report_id)
+		@user = user
+		@message = message
+		@url = report.pdf_url
+		mail(to: @user.email, subject: subject)
 	end
 
 	def inspection_email(inspection_id, user, subject, message)
@@ -83,13 +62,4 @@ class UserSendGridMailer < ApplicationMailer
 		@url = inspection.pdf_url
 		mail(to: @user.email, subject: subject)
 	end
-
-	def report_email(report_id, user, subject, message)
-		report = Report.find(inspection_id)
-		@user = user
-		@message = message
-		@url = report.default_pdf
-		mail(to: @user.email, subject: subject)
-	end
-
 end
