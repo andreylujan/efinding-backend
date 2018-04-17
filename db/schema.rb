a3dbcfc1328f0d7f65bce888d43fea0cf7634c54
Santiago Morales <smorales@bildchile.com>
2018-04-17 14:13:44 -0300

    add roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322144117) do
+ActiveRecord::Schema.define(version: 20180403150745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -534,6 +534,7 @@ ActiveRecord::Schema.define(version: 20180322144117) do
     t.integer  "role_id",                             null: false
     t.datetime "deleted_at"
     t.json     "constructions",          default: {}, null: false
+    t.json     "roles",                  default: {}, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
