9df200db43dd7556a042ed785586881e9ca0772b
Alejandro Rodriguez <alejandrorodriguezpena@gmail.com>
2018-05-14 12:29:56 -0400

    fix push 2

--- app/models/report.rb
+++ app/models/report.rb
@@ -550,10 +550,102 @@ class Report < ApplicationRecord
   def change_state
     unless self.ignore_state_changes
       if self.creator.organization_id == 4 and self.state_changed?
-        ChangeStateJob.perform_now(self.id.to_s)
+        self._changeStatus()
       end
       if self.creator.organization_id == 3 and self.state_changed?
         regenerate_pdf(true)
+      end
+    end
+  end
+
+  def _changeStatus
+    report = Report.find(self.id)
+    order_id = report.dynamic_attributes.dig("49", "text").to_i
+    state = ""
+    message = nil
+    Rails.logger.info "Estado : #{report.state}"
+    if report.state == "accepted"
+      state = "pedido aceptado"
+    elsif report.state == "rejected"
+      state = "pedido rechazado"
+      message = report.get_message
+    elsif report.state == "confirming_suggestions"
+      state = "pedido con observacion"
+      message = report.get_message
+    elsif report.state == "delivering"
+      state = "pedido en camino"
+    elsif report.state == "delivered"
+      state = "pedido entregado"
+    elsif report.state == "awaiting_delivery"
+      state = "Pendiente de retiro"
+    else
+      return
+    end
+    conn = Faraday.new(:url => "http://ec2-54-88-114-83.compute-1.amazonaws.com")
+    body = {
+      id_order: order_id,
+      state: state
+    }
+    if message.present?
+      body[:message] = message
+    end
+    begin
+      response = conn.post do |req|
+        req.url '/delivery_api/public/index.php/api/v1/detail_change_state'
+        req.headers['Content-Type'] = 'application/json'
+        req.body = body.to_json
+      end
+      RequestLog.create!(
+        organization_id: 4,
+        url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/delivery_api/public/index.php/api/v1/detail_change_state",
+        status_code: response.status,
+      response_body: response.body)
+    rescue => e
+      RequestLog.create!(
+        organization_id: 4,
+        url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/delivery_api/public/index.php/api/v1/detail_change_state",
+        status_code: 0,
+        error_messages: {
+          message: e.message
+      })
+    end
+
+    if report.state == "awaiting_delivery"
+      Rails.logger.info "REPORTS PUSH: #{report.state}"
+
+      Rails.logger.info "ORDER STATE Pendiente de retiro - creator_id: #{report.creator_id}"
+      DeliverySendTaskJob.perform_now(report.id.to_s,
+                                                  "Pedido Pendiente de retiro",
+                                                 "El pedido #{order_id} está Pendiente de retiro", report.get_delivery_devices)
+
+    end
+
+    if report.state == "accepted"
+      conn = Faraday.new(:url => "http://ec2-54-88-114-83.compute-1.amazonaws.com")
+      body = {
+        id_order: order_id,
+        state: state
+      }
+      begin
+        response = conn.post do |req|
+          req.url '/delivery_api/public/index.php/pago/pagar'
+          req.body = {
+            order_id: order_id
+          }
+        end
+        RequestLog.create!(
+          organization_id: 4,
+          url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/delivery_api/public/index.php/pago/pagar",
+          status_code: response.status,
+        response_body: response.body)
+      rescue => e
+        RequestLog.create!(
+          organization_id: 4,
+          url: "http://ec2-54-88-114-83.compute-1.amazonaws.com/delivery_api/public/index.php/pago/pagar",
+          status_code: 0,
+          error_messages: {
+            message: e.message
+        })
       end
     end
   end
